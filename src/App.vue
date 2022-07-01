441a356362f36c89384b11091dea472380fd6216
waitForBlooming <1158623039@qq.com>
2022-07-01 22:13:53 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div id="app">
+  <div id='app'>
+    <h1>黑马头条</h1>
+    <i class='iconfont iconfont-ku'></i>
+    <i class='iconfont iconfont-sousuo'></i>
+    <van-button type="info">信息按钮</van-button>
     <!-- 路由的出口 -->
     <router-view />
   </div>
@@ -10,4 +14,4 @@ export default {
 }
 </script>
 
-<style lang="less"></style>
+<style lang='less'></style>
